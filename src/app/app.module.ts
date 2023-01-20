e23b474db7109b1c65341125fbc3e267e570fc6c
Idana Wagner <idanawagner@gmail.com>
2023-01-19 21:07:53 -0300

    generated components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StudentsComponent } from './components/students/students.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentsComponent,
+    NavbarComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule
